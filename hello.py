70ce0677fce90ad8cca3b5d347dda4745ea796e5
moazam <moazamyasintips@gmail.com>
2026-07-16 12:14:49 +0500

    Changed in hello.py file by simple guessing game

--- hello.py
+++ hello.py
@@ -1,8 +1,39 @@
-"""Hello code prepared by Moazam."""
-
-def main():
-    print("Hello, World!")
+student_name = "moazam"
+ag_no = 9343
 
 
-if __name__ == "__main__":
-    main()
+name_input = input("Student Name: ")
+ag_input = input("Enter Your Ag: ")
+class_input = input("Enter Your Class Name: ")
+
+if name_input == student_name:
+
+    print("Login Successful")
+else:
+    print("Login Unsuccessful")
+
+print("welcome to", student_name,"in our paradise")
+
+print("Let's play a game!")
+
+while True:
+    user = input("Guess the number from 1 to 4: ")
+
+    if user == "1":
+        print("You are intelligent!")
+    elif user == "2":
+        print("Nice try! You are improving.")
+    elif user == "3":
+        print("Good choice! You are learning fast.")
+    elif user == "4":
+        print("Almost there! Keep going.")
+    else:
+        print("Invalid number, please choose between 1 and 4.")
+
+    # Ask if the user wants to play again
+    choice = input("Do you want to play again? (yes/no): ").lower()
+
+    if choice != "yes":
+        print("Thanks for playing! See you next time.")
+        break
+
